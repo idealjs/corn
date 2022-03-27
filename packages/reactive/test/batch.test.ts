62d699305cba884b93855d771d312c62029e15ee
cqh963852 <cqh963852@hotmail.com>
2022-03-27 16:09:29 +0800

    update: update test case

--- packages/reactive/test/batch.test.ts
+++ packages/reactive/test/batch.test.ts
@@ -1,50 +1,42 @@
 import Reactive from "../src/Reactive";
-import timer from "./timer";
+import waitUntil from "./waitUntil";
 
 const reactive = new Reactive();
 
-beforeEach(() => {
-  jest.useFakeTimers();
-});
-
 describe("batch test", () => {
-  test("test 1", (done) => {
+  test("test 1", async () => {
     const [name, setName] = reactive.createSignal<string>();
     const [result, setResult] = reactive.createSignal<string | undefined>(
       name()
     );
+
+    let count = 0;
+
+    const effectFn = jest.fn(() => {
+      setResult(name());
+      count = count + 1;
+    });
+
+    const name1 = "world";
+    const name2 = "world world";
+
     reactive.createRoot(() => {
-      let count = 0;
-
-      const effectFn = jest.fn(() => {
-        setResult(name());
-        count = count + 1;
-      });
-
       reactive.useEffect(effectFn);
 
       expect(count).toBe(1);
-
-      const name1 = "world";
-      const name2 = "world world";
 
       reactive.batch(() => {
         setName(name1);
         setName(name2);
       });
-
-      jest.advanceTimersByTime(16);
-      timer(16)
-        .then(() => {
-          expect(effectFn).toBeCalledTimes(2);
-          expect(result()).toBe(name2);
-          expect(count).toBe(2);
-          done();
-        })
-        .catch((err) => {
-          done(err);
-        });
-      jest.advanceTimersByTime(16);
     });
+
+    await waitUntil(() => {
+      return !reactive["scheduler"].working;
+    });
+
+    expect(effectFn).toBeCalledTimes(2);
+    expect(result()).toBe(name2);
+    expect(count).toBe(2);
   });
 });
