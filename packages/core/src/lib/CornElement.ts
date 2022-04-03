d1dc0432310023bf56cbaff28e07f05ab8530b6e
cqh963852 <cqh963852@hotmail.com>
2022-04-04 00:49:39 +0800

    update: Add clean

--- packages/core/src/lib/CornElement.ts
+++ packages/core/src/lib/CornElement.ts
@@ -27,47 +27,35 @@ export interface Props extends IEvents {
   key?: Key;
 }
 
-const handleChildren = (children: Child[]): VNode[] => {
+const handleChildren = (children: Child[], clean?: () => void): VNode[] => {
   return children
     ?.flatMap((child) => {
       if (isCornText(child)) {
         return child.toString();
       }
       if (child instanceof CornElement) {
-        return child.create();
+        return child.create(true, clean);
       }
       if (child instanceof Function) {
         if (Reflect.getOwnPropertyDescriptor(child, IS_REACTIVE_READ)?.value) {
-          const handleChildEffectFn = (prev?: VNode) => {
+          return useEffect(() => {
             const res = child();
-            console.debug("[debug] effectFn res", res, prev);
+            console.debug("[debug] effectFn res", res);
             if (isCornText(res)) {
-              if (prev == null) {
-                const vNode = h("div", [res.toString()]);
-                return vNode;
-              } else {
-                const vNode = h("div", [res.toString()]);
-                return patch(prev, vNode);
-              }
+              const vNode = h("div", [res.toString()]);
+              return vNode;
             }
             if (res instanceof Array) {
-              if (prev == null) {
-                const resChildren = handleChildren(res);
-                const vNode = h("div", resChildren);
-                return vNode;
-              } else {
-                const resChildren = handleChildren(res);
-                const vNode = h("div", resChildren);
-                return patch(prev, vNode);
-              }
+              const resChildren = handleChildren(res, clean);
+              const vNode = h("div", resChildren);
+              return vNode;
             }
             console.warn("[warn] skip create res", res, typeof res);
-          };
-          return useEffect<VNode | undefined>(handleChildEffectFn);
+          });
         }
       }
       if (child instanceof Array) {
-        return handleChildren(child);
+        return handleChildren(child, clean);
       }
       console.warn("[warn] skip create child", child, typeof child);
       return null;
@@ -78,35 +66,45 @@ const handleChildren = (children: Child[]): VNode[] => {
 class CornElement {
   private type: string | ((props: Props) => CornElement);
   private props: Props;
+  private clean?: () => void;
   constructor(type: string | ((props: Props) => CornElement), props: Props) {
     this.type = type;
     this.props = props;
+    this.create = this.create.bind(this);
   }
 
-  public create(withRoot: boolean = true): VNode {
+  public create(withRoot: boolean = true, clean?: () => void): VNode {
     const effectFn = (prev?: VNode) => {
       if (this.type instanceof Function) {
         if (prev != null) {
           return prev;
         }
         const cornElement = this.type(this.props);
-        const vNode = cornElement.create(false);
+        const vNode = cornElement.create(false, this.clean || clean);
 
         return vNode;
       }
 
-      const children = handleChildren([this.props.children].flatMap((c) => c));
+      const children = handleChildren(
+        [this.props.children].flatMap((c) => c),
+        this.clean || clean
+      );
 
       const vNode = h(
         this.type,
         { ...this.props, on: { click: this.props.onClick } },
         children
       );
-
-      return vNode;
+      if (prev) {
+        return patch(prev, vNode);
+      } else {
+        return vNode;
+      }
     };
     if (withRoot) {
-      return createRoot(() => {
+      clean && clean();
+      return createRoot((dispose) => {
+        this.clean = dispose;
         return useEffect(effectFn);
       });
     } else {
