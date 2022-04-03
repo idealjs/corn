543d601d52473ac508937fd707f2d0cfc5db4d0a
cqh963852 <cqh963852@hotmail.com>
2022-04-03 21:23:31 +0800

    update: Add new test case

--- packages/reactive/test/useEffect.nested.test.ts
+++ packages/reactive/test/useEffect.nested.test.ts
@@ -52,6 +52,57 @@ describe("global signal, nested useEffect", () => {
     });
   });
 
+  test("global signal, nested useEffect, effect return value", (done) => {
+    const [name, setName] = reactive.createSignal<string>();
+    let count = 0;
+    const name1 = "world";
+    const name2 = "world world";
+
+    const effectInner = jest
+      .fn(() => {
+        name();
+        count = count + 1;
+        return count;
+      })
+      .mockName("effectInner");
+
+    const effectOuter = jest
+      .fn((prev?: number) => {
+        if (prev != null && prev >= 1) {
+          return 2;
+        }
+        return reactive.useEffect(effectInner);
+      })
+      .mockName("effectOuter");
+
+    const testCase = async () => {
+      setName(name1);
+      await waitUntil(() => {
+        return !reactive["scheduler"].working;
+      });
+      expect(effectOuter).toBeCalledTimes(1);
+      expect(effectInner).toBeCalledTimes(2);
+
+      setName(name2);
+      await waitUntil(() => {
+        return !reactive["scheduler"].working;
+      });
+      expect(effectOuter).toBeCalledTimes(1);
+      expect(effectInner).toBeCalledTimes(3);
+    };
+
+    reactive.createRoot(() => {
+      reactive.useEffect(effectOuter);
+      testCase()
+        .then(() => {
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+  }, 20000);
+
   test("global signal, nested root/useEffect ", (done) => {
     const [name, setName] = reactive.createSignal<string>();
     let count = 0;
